a337bfa001244519611f22539666eb027157bfb7
Suzy-Lee <mulddang2@naver.com>
2023-08-29 14:59:22 +0900

    Fix: update Header component

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,42 +5,46 @@ import Space from './components/Space';
 import { GoEye } from 'react-icons/go';
 import { AiOutlineFork } from 'react-icons/ai';
 import { BsStar } from 'react-icons/bs';
+import Tabs from './components/Tabs';
 
 export default function Header() {
   return (
     <div className={styles.header}>
-      <Button
-        style={{
-          fontSize: '14px',
-          backgroundColor: 'transparent',
-          color: 'black',
-        }}
-      >
-        <GoEye style={{ marginRight: '8px' }} />
-        Watch
-      </Button>
-      <Space />
-      <Button
-        style={{
-          fontSize: '14px',
-          backgroundColor: 'transparent',
-          color: 'black',
-        }}
-      >
-        <AiOutlineFork style={{ marginRight: '8px' }} />
-        Fork <div className={styles.circle}>5</div>
-      </Button>
-      <Space />
-      <Button
-        style={{
-          fontSize: '14px',
-          backgroundColor: 'transparent',
-          color: 'black',
-        }}
-      >
-        <BsStar style={{ marginRight: '8px' }} />
-        Star
-      </Button>
+      <div className={styles.buttonContainer}>
+        <Button
+          style={{
+            fontSize: '14px',
+            backgroundColor: 'transparent',
+            color: 'black',
+          }}
+        >
+          <GoEye style={{ marginRight: '8px' }} />
+          Watch
+        </Button>
+        <Space />
+        <Button
+          style={{
+            fontSize: '14px',
+            backgroundColor: 'transparent',
+            color: 'black',
+          }}
+        >
+          <AiOutlineFork style={{ marginRight: '8px' }} />
+          Fork <div className={styles.circle}>5</div>
+        </Button>
+        <Space />
+        <Button
+          style={{
+            fontSize: '14px',
+            backgroundColor: 'transparent',
+            color: 'black',
+          }}
+        >
+          <BsStar style={{ marginRight: '8px' }} />
+          Star
+        </Button>
+      </div>
+      <Tabs />
     </div>
   );
 }
