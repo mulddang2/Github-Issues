0703f55b9d687f4f48ae6864ef80878bf2ff4be3
Suzy-Lee <mulddang2@naver.com>
2023-08-29 16:51:32 +0900

    Feat: add Input UI
    input UI 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 /* eslint-disable react/prop-types */
 import styles from './App.module.css';
-
-import Button from './components/Button';
 import Header from './Header';
+import ListContainer from './ListContainer';
 
 function App() {
   return (
     <>
       <div className={styles.nav}>Nav</div>
       <Header />
-      <div className={styles.listContainer}>
-        <Button
-          style={{ fontSize: '14px', backgroundColor: 'green', color: 'white' }}
-        >
-          New Issue
-        </Button>
-      </div>
+      <ListContainer />
       <div className={styles.footer}>Footer</div>
     </>
   );
---
+++ src/ListContainer.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import styles from './ListContainer.module.css';
+import Button from './components/Button';
+
+export default function ListContainer() {
+  const [inputValue, setInputValue] = useState('is:pr is:open');
+
+  useEffect(() => {
+    console.log({ inputValue });
+  }, [inputValue]);
+
+  return (
+    <div className={styles.listContainer}>
+      <div className={styles.topSection}>
+        <input
+          className={styles.input}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Button
+          style={{ fontSize: '14px', backgroundColor: 'green', color: 'white' }}
+        >
+          New Issue
+        </Button>
+      </div>
+    </div>
+  );
+}
