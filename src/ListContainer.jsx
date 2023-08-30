e049dd4a9e67d0a2a9f57d85d545836ef72a58e1
Suzy-Lee <mulddang2@naver.com>
2023-08-30 12:23:51 +0900

    Feat: add mode ( open, closed )

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -1,11 +1,17 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react';
 import styles from './ListContainer.module.css';
 import Button from './components/Button';
 import ListItem from './components/ListItem';
 import ListItemLayout from './components/ListItemLayout';
+import cx from 'clsx';
 
 export default function ListContainer() {
   const [inputValue, setInputValue] = useState('is:pr is:open');
+  const [isOpenMode, setIsOpenMode] = useState(true);
+
+  const openModeDataSize = 1;
+  const closeModeDataSize = 2;
 
   return (
     <div className={styles.listContainer}>
@@ -21,6 +27,12 @@ export default function ListContainer() {
           New Issue
         </Button>
       </div>
+      <>
+        <OpenClosedFilter size={openModeDataSize} state='Open' onClick={() => setIsOpenMode(true)} selected={isOpenMode}  />
+        <OpenClosedFilter size={closeModeDataSize} state='Closed' onClick={() => setIsOpenMode(false)} selected={!isOpenMode}  />
+      </>
+        
+      
       <ListItemLayout className={styles.listFilter}>
         <div className={styles.filterLists}>
           <span>Author</span>
@@ -32,11 +44,29 @@ export default function ListContainer() {
         </div>
       </ListItemLayout>
       <div className={styles.container}>
-        <ListItem badges={[{
-          color: 'red',
-          title: 'bug',
-        }]}/>
+        <ListItem
+          // checked={checkedList.filter((item) => item.id == '0')[0]}
+          // onChangeCheckBox={() => setCheckedList((checkedList) => [...checkedList, '0'])}
+          badges={[
+            {
+              color: 'red',
+              title: 'bug',
+            },
+          ]}
+        />
       </div>
     </div>
+  );
+}
+
+function OpenClosedFilter({ size, state, onClick, selected }) {
+  return (
+    <span
+      role="button"
+      className={cx(styles.textFilter, { [styles.selected]: selected })}
+      onClick={onClick}
+    >
+      {size} {state}
+    </span>
   );
 }
