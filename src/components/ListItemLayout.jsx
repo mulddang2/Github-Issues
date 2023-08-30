4cc8c41ed20a8e8a9918d949a652ff5c5d8c1bd4
Suzy-Lee <mulddang2@naver.com>
2023-08-30 11:09:45 +0900

    Feat: add ListItem layout component

---
+++ src/components/ListItemLayout.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import cx from 'clsx'
+import styles from './ListItemLayout.module.css';
+
+export default function ListItemLayout({ children, className }) {
+  return (
+    <div className={cx(className, styles.wrapper)}>
+      <input
+        type="checkbox"
+        className={styles.checkbox}
+        //value={checked}
+        //onChange={onChangeCheckBox}
+      />
+      {children}
+    </div>
+  );
+}
