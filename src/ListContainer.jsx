ba7725284ddd40af3fa9b766bfd5e74d503c263d
Suzy-Lee <mulddang2@naver.com>
2023-08-30 11:10:12 +0900

    Fix: update List container

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import styles from './ListContainer.module.css';
 import Button from './components/Button';
+import ListItem from './components/ListItem';
+import ListItemLayout from './components/ListItemLayout';
 
 export default function ListContainer() {
   const [inputValue, setInputValue] = useState('is:pr is:open');
@@ -18,6 +20,19 @@ export default function ListContainer() {
         >
           New Issue
         </Button>
+      </div>
+      <ListItemLayout className={styles.listFilter}>
+        <div className={styles.filterLists}>
+          <span>Author</span>
+          <span>Label</span>
+          <span>Projects</span>
+          <span>Milestones</span>
+          <span>Assignee</span>
+          <span>Sort</span>
+        </div>
+      </ListItemLayout>
+      <div className={styles.container}>
+        <ListItem />
       </div>
     </div>
   );
