75706de52247029f62d16a5e587efd788645d9f7
Suzy-Lee <mulddang2@naver.com>
2023-09-04 23:58:36 +0900

    Feat: add Pagination component

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/prop-types */
+import styles from './Pagination.module.css';
+import cx from 'clsx';
+
+export default function Pagination({
+  currentPage,
+  maxPage,
+  onClickPageButton,
+}) {
+  return (
+    <div>
+      <button
+        className={cx(styles.button, styles.blueButton)}
+        disabled={currentPage === 1}
+      >
+        {'< Previous'}
+      </button>
+      {new Array(maxPage).fill(null).map((_, i) => (
+        <PageButton key={i + 1}
+          number={i + 1}
+          onClick={onClickPageButton}
+          selected={i + 1 === currentPage}
+        />
+      ))}
+      <button
+        className={cx(styles.button, styles.blueButton)}
+        disabled={currentPage === maxPage}
+      >
+        {'Next >'}
+      </button>
+    </div>
+  );
+}
+
+function PageButton({ number, onClick, selected }) {
+  return (
+    <button
+      className={cx(styles.button, { [styles.selected]: selected })}
+      onClick={() => onClick(number)}
+    >
+      {number}
+    </button>
+  );
+}
