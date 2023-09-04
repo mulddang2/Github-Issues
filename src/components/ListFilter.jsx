3167f2b299f46b59ad64982cc9ad1c3e30ec61ec
Suzy-Lee <mulddang2@naver.com>
2023-09-04 23:58:08 +0900

    Feat: add ListFilter component

---
+++ src/components/ListFilter.jsx
@@ -0,0 +1,43 @@
+import { IoMdArrowDropdown } from 'react-icons/io';
+import styles from './ListFilter.module.css';
+import { useState } from 'react';
+import Modal from './Modal';
+
+export default function ListFilter({ onChangeFilter }) {
+  return (
+    <>
+      <div className={styles.filterLists}>
+        <ListFilterItem>Author</ListFilterItem>
+        <ListFilterItem>Label</ListFilterItem>
+        <ListFilterItem>Projects</ListFilterItem>
+        <ListFilterItem>Milestones</ListFilterItem>
+        <ListFilterItem>Assignee</ListFilterItem>
+        <ListFilterItem>Sort</ListFilterItem>
+      </div>
+    </>
+  );
+}
+
+function ListFilterItem({ onClick, children, onChangeFilter }) {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <div className={styles.filterItem}>
+      <span role="button" onClick={() => setShowModal(true)}>
+        {children} <IoMdArrowDropdown />
+      </span>
+      <div className={styles.modalContainer}>
+        <Modal
+          opened={showModal}
+          onClose={() => setShowModal(false)}
+          placeholder="Filter labels"
+          searchDataList={['bug', 'Labels', 'Apple']}
+          onClickCell={() => {
+            // 클릭된 정보를 통해 리스트 필터링
+            onChangeFilter();
+          }}
+        />
+      </div>
+    </div>
+  );
+}
