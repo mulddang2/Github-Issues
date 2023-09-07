674fb4484381307510726d5eceba8f808b3cd38f
Suzy-Lee <mulddang2@naver.com>
2023-09-07 23:22:19 +0900

    Refactor: ListdFilter component

--- src/components/ListFilter.jsx
+++ src/components/ListFilter.jsx
@@ -1,37 +1,61 @@
+/* eslint-disable react/prop-types */
 import { IoMdArrowDropdown } from 'react-icons/io';
 import styles from './ListFilter.module.css';
 import { useState } from 'react';
 import Modal from './Modal';
 
 export default function ListFilter({ onChangeFilter }) {
+  const [showModal, setShowModal] = useState();
+  const filterList = [
+    'Author',
+    'Label',
+    'Projects',
+    'Milestones',
+    'Assignee',
+    'Sort',
+  ];
+
   return (
     <>
       <div className={styles.filterLists}>
-        <ListFilterItem>Author</ListFilterItem>
-        <ListFilterItem>Label</ListFilterItem>
-        <ListFilterItem>Projects</ListFilterItem>
-        <ListFilterItem>Milestones</ListFilterItem>
-        <ListFilterItem>Assignee</ListFilterItem>
-        <ListFilterItem>Sort</ListFilterItem>
+        {filterList.map((filter) => (
+          <ListFilterItem
+            key={filter}
+            searchDataList={[]}
+            onClick={() => setShowModal(filter)}
+            onClose={() => setShowModal()}
+            showModal={showModal === filter}
+          >
+            {filter}
+          </ListFilterItem>
+        ))}
       </div>
     </>
   );
 }
 
-function ListFilterItem({ onClick, children, onChangeFilter }) {
-  const [showModal, setShowModal] = useState(false);
+function ListFilterItem({
+  onClick,
+  onClose,
+  searchDataList,
+  children,
+  placeholder,
+  onChangeFilter,
+  showModal,
+}) {
+  // const [showModal, setShowModal] = useState(false);
 
   return (
     <div className={styles.filterItem}>
-      <span role="button" onClick={() => setShowModal(true)}>
+      <span role="button" onClick={onClick}>
         {children} <IoMdArrowDropdown />
       </span>
       <div className={styles.modalContainer}>
         <Modal
+          placeholder={placeholder}
           opened={showModal}
-          onClose={() => setShowModal(false)}
-          placeholder="Filter labels"
-          searchDataList={['bug', 'Labels', 'Apple']}
+          onClose={onClose}
+          searchDataList={searchDataList}
           onClickCell={() => {
             // 클릭된 정보를 통해 리스트 필터링
             onChangeFilter();
